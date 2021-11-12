f78439620d8cc84d9f6c0d2628cd9c313d19ac2d
Tom Biato <tom.biato@ynov.com>
2021-11-12 09:59:40 +0100

    Init project

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.get('/ping', (req, res) => {
+	res.send('Pong');
+});
+
+app.post('/api/supply', (req, res) => {
+	// TODO
+});
+
+app.get('/api/supply/summary', (req, res) => {
+	// TODO
+});
+
+app.post('/api/supply-needed', (req, res) => {
+	// TODO
+});
+
+app.post('/api/supply-request', (req, res) => {
+	// TODO
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
